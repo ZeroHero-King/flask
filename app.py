d3d6de353e7b78ddc2f900547cb00eefce99c2d1
ZeroHero <rzabekov.abay@gmail.com>
2024-02-06 16:40:46 +0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py
    	modified:   templates/index.html
    	modified:   templates/layout.html
    	modified:   templates/login.html
    	modified:   templates/register.html
    	modified:   templates/todo-list.html

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/todo')
+def todo():
+    username = request.args.get('username')
+    return render_template('todo_list.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
