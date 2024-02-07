e23f477d1d6a92ec884adc61b22c61f90e7cbe94
ZeroHero <rzabekov.abay@gmail.com>
2024-02-07 09:52:34 +0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py
    	modified:   readme.MD
    	renamed:    static/database/users.sqlite3 -> static/database/database.sqlite3
    	modified:   static/database/db.py
    	modified:   templates/index.html
    	modified:   templates/login.html
    	modified:   templates/register.html
    	modified:   templates/todo-list.html

--- app.py
+++ app.py
@@ -5,20 +5,61 @@ app = Flask(__name__) # создаем объект класса Flask
 
 @app.route('/')
 def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        user = cursor.fetchone()
+        if user:
+            return render_template('todo_list.html', username=username)
+        else:
+            return render_template('login.html')
     return render_template('index.html')
 
 @app.route('/login')
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        user = cursor.fetchone()
+        if user:
+            return render_template('todo_list.html', username=username)
+        else:
+            return render_template('login.html')
     return render_template('login.html')
 
 @app.route('/register')
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', (username, email, password))
+        conn.commit()
+        conn.close()
+        return render_template('todo_list.html', username=username)
     return render_template('register.html')
 
 @app.route('/todo')
 def todo():
+    if request.method == 'POST':
+        task = request.form['task']
+        username = request.form['username']
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO tasks (task, username) VALUES (?, ?)', (task, username))
+        conn.commit()
+        conn.close()
+        return render_template('todo_list.html', username=username)
     username = request.args.get('username')
     return render_template('todo_list.html', username=username)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- static/database/db.py
+++ static/database/db.py
@@ -1,16 +1,24 @@
 import sqlite3
 
 # Создаем подключение к базе данных
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('static/database/database.sqlite3')
 cursor = conn.cursor()
 
-# Создаем таблицу пользователей
+# Создаем таблицу задач
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL UNIQUE,
-        email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username INTEGER NOT NULL,
+        task TEXT NOT NULL UNIQUE,
+        status BOOLEAN NOT NULL,
+        FOREIGN KEY (username) REFERENCES users(username)
     )
 ''')
 
